4fed963dfb7eab37f38ec7f890d7db6ddf30305a
Janice <janicenguyenn@gmail.com>
2023-07-12 11:34:27 -0700

    add code to create new board
    Co-authored-by: Anna, Susi <annavanwesep@users.noreply.github.com>

---
+++ src/components/NewBoardForm.js
@@ -0,0 +1,72 @@
+import React, { useState} from "react";
+import PropTypes from 'prop-types';
+import './NewBoardForm.css';
+
+const INITIAL_FORM_DATA = {
+    title: '',
+    description: '',
+    owner: ''
+};
+
+const NewBoardForm = (props) => {
+    const [boardFormData , setBoardFormData] = useState(INITIAL_FORM_DATA);
+
+    const anInputChanged = (event) => {
+        const newBoardFormData = {
+            ...boardFormData, 
+                [event.target.name]: event.target.value
+
+            };
+            setBoardFormData(newBoardFormData);
+        };
+
+    const handleFormSubit = (event) => {
+        event.preventDefault();
+        props.createNewBoard(boardFormData);
+        setBoardFormData(INITIAL_FORM_DATA);
+    };
+
+    return (
+        <section className={props.showHide}>
+            <h3>Create New Board</h3>
+            <form onSubmit={handleFormSubmit}>
+                {/* form board title */}
+                <label htmlFor='boardName'>Board Title:</label>
+                <input
+                    id='boardTitle'
+                    name='title'
+                    type='text'
+                    value= { anInputChanged }
+                />
+                {/* form board description */}
+                <label htmlFor='boardDescription'>Description:</label>
+                <input
+                    id='boardDescription'
+                    name='description'
+                    type='text'
+                    value={ boardFormData.description }
+                    onChange={ anInputChanged }
+                />
+                {/* form board owner */}
+                <label htmlFor='boardOwner'>Owner:</label>
+                <input
+                id='boardOwner'
+                name='owner'
+                type='text'
+                value= {boardFormData.owner }
+                onChange= { anInputChanged }
+                />
+                {/* form add new board */}
+                <input type='submit' value='Add new board'></input>
+            </form>
+        </section>
+    );
+};
+
+
+NewBoardForm.propTypes = {
+    showHideCards:PropTypes.string.isRequired,
+    createNewBoard: PropTypes.func.isRequired,
+};
+
+export default NewBoardForm;
