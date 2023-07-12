a606f4eff82aa38cc28b26cbe1ab27b7b54b793c
Janice <janicenguyenn@gmail.com>
2023-07-12 11:50:02 -0700

    add imports

--- src/components/Board.js
+++ src/components/Board.js
@@ -2,6 +2,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './Board.css';
 import CardList from './CardList';
+import axios from 'axios';
+import { selectedBoard } from './App'
 
 
 
