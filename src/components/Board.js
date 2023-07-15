6640a8cf28320f36fec8b777b3887869d94cb065
Anna Lima <annavanwesep@gmail.com>
2023-07-15 15:17:24 -0700

    fixed routes and useeffect to show cards for each board

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,42 +1,44 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import './Board.css';
 import CardList from './CardList';
 import axios from 'axios';
 // import App from '../App'
 
-const Board = ({selectedBoard}, props) => {
-  
+const Board = (props) => {
   //Render initial address
   const RENDER_URL = 'https://asj-forever-inspiration.onrender.com/boards'
 
   //create card state, gets updated through api call
-
   const [cards, setCards] = useState([])
-
 
   // get all cards from one board
   const loadCards = (boardId) => {
-  
-    axios.get(`${RENDER_URL}/${boardId}/cards`)
-    .then((response)=> {
-      const cardCopy = response.data.map((card)=> {
-        return {
-          id: card.card_id,
-          message: card.message,
-          likesCount:  card.likes_count,
-        };  
-      });
-      setCards(cardCopy);
-    })
+    console.log(`Loading cards for board ${boardId}!`);
+    axios
+      .get(`${RENDER_URL}/${boardId}/cards`)
+      .then((response)=> {
+        const cardCopy = response.data.map((card)=> {
+          return {
+            id: card.card_id,
+            message: card.message,
+            likesCount:  card.likes_count,
+          };  
+        });
+        setCards(cardCopy);
+      })
 
-    .catch((error)=>{
-      console.log('error', error)
-    })
-
+      .catch((error)=>{
+        console.log('error', error)
+      })
   };
-  loadCards(selectedBoard.boardId)
-  console.log(cards)
+
+  useEffect(() => {
+    console.log(props);
+    if (props.selectedBoard) {
+      loadCards(props.selectedBoard.board_id);
+    }
+  }, [props.selectedBoard]);
   
   // Move the updateLike function -- double check to ensure same functionality
   const updateLike = (cardId) => {
@@ -58,13 +60,13 @@ const Board = ({selectedBoard}, props) => {
       .catch((error) => {
         console.error(error.response.data.message);
       });
-      };
+    };
 
       // Create a card function needs to get moved
  const createNewCard = (message) => {
   axios
     .post(
-      `${RENDER_URL}/boards/${selectedBoard.board_id}/cards`,
+      `${RENDER_URL}/boards/${props.selectedBoard.board_id}/cards`,
       {message}
     )
     .then((result) => {
@@ -83,7 +85,7 @@ const Board = ({selectedBoard}, props) => {
     .catch((error) => console.log(error.response.data));
   };
 
-// Move delete card function
+  // Move delete card function
   const updateDelete = (cardId) => {
     axios
       .delete(`${RENDER_URL}/cards/${cardId}`)
@@ -98,7 +100,8 @@ const Board = ({selectedBoard}, props) => {
       console.error(error.response.data.message);
     });
   };
-// Pass in all props to cardlist --> card
+
+  // Pass in all props to cardlist --> card
   return (
     <section>
       <h2>{props.title}</h2>
@@ -106,11 +109,8 @@ const Board = ({selectedBoard}, props) => {
       <span className='card__list'>
         <CardList 
           boardId={props.id} 
-          loadCardsProp={props.loadCards}
-          createNewCardProp={props.createNewCard}
-          updateDeleteProp={props.updateDelete}
-          updateLikeProp={props.updateLike}
-          cards = {cards}
+          loadCards={loadCards}
+          cards={cards}
           createNewCard={createNewCard}
           updateDelete={updateDelete}
           updateLike={updateLike}
@@ -121,12 +121,9 @@ const Board = ({selectedBoard}, props) => {
   };
 
 Board.propTypes = {
-  board: PropTypes.shape({
-      id: PropTypes.number.isRequired,
-      title: PropTypes.string.isRequired,
-      owner: PropTypes.string.isRequired
-    }),
-  
-  };
+  selectedBoard: PropTypes.shape({
+    boardId: PropTypes.number.isRequired,
+  })
+};
 
 export default Board;
